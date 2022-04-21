45ea28043fabe2a84fa2b6c13866cc42122b47f0
Franco Raffaele <fran.raffa15@gmail.com>
2022-04-21 10:37:17 -0300

    Initial commit

---
+++ Assets/ObjectsClone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsClone : MonoBehaviour
+{
+
+    public int amount;
+    public Transform objectToClone;
+    int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter < amount)
+        {
+            Instantiate(objectToClone);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/While01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+
+    public int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter <= 100)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //counter++;
+        //Debug.Log("Pasó el frame número"+ counter);
+
+    }
+}
